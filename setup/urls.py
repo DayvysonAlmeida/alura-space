563d52e44f659656250e095d5c53c36e16946f3f
Dayvyson Almneida <dayvysonalmeida@gmail.com>
2024-07-16 14:58:15 -0300

    criação de uma nona urls no app e ajuste na da setup

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from galeria.views import index, imagem
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('imagem/', imagem, name='imagem')
+    path('', include('galeria.urls')),
 ]
